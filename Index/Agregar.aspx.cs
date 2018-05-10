1a7083a6b61a281aa5110b306a4b7c42046b91f3
Juan de Dios Canche Cen <juan1910@live.com.mx>
2018-05-09 22:44:45 -0500

    cambios en txt

--- Index/Agregar.aspx.cs
+++ Index/Agregar.aspx.cs
@@ -23,11 +23,11 @@ namespace Index
         protected void btnEnviar_Click(object sender, EventArgs e)
         {
             
-            String nombre = this.Request.Form["txtNombre"];
-            String descripcion = this.Request.Form["txtDescipcion"]; 
+            String nombre = this.Request.Form["nombre"];
+            String descripcion = this.Request.Form["descripcion"]; 
             String tipo = this.Request.Form["tipo"];
             String zonas = this.Request.Form["zonas"];
-            String presion = this.Request.Form["precion"];
+            String presion = this.Request.Form["presion"];
             String tratamiento = this.Request.Form["txtTratamiento"]; 
             String precio = this.Request.Form["txtPrecio"];
 
